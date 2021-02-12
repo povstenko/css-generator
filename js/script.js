f6b0673a54cc381f40a0e99a1ec20fe967397796
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-02-12 14:00:27 +0200

    border radius change

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function func() {
+    // alert();
+    let tl_r = document.getElementById('tl-range').value;
+    let tr_r = document.getElementById('tr-range').value;
+    let bl_r = document.getElementById('bl-range').value;
+    let br_r = document.getElementById('br-range').value;
+
+    let tl_t = document.getElementById('tl-text');
+    let tr_t = document.getElementById('tr-text');
+    let bl_t = document.getElementById('bl-text');
+    let br_t = document.getElementById('br-text');
+
+    let ex = document.getElementById('example');
+
+    tl_t.value=tl_r;
+    tr_t.value=tr_r;
+    bl_t.value=bl_r;
+    br_t.value=br_r;
+
+    ex.style.borderRadius= tr_r+'px '+tl_r+'px '+br_r+'px '+bl_r+'px';
+}
